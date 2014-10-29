d8e1ab2db4636b87f7443f79d20d586a17999ec5
greenteamer <greenteamer@bk.ru>
2014-10-29 23:30:42 +0300

    система купонов + раздел скидки

--- webshop/cart/cart.py
+++ webshop/cart/cart.py
@@ -5,11 +5,108 @@ import random
 
 from django.shortcuts import get_object_or_404
 
-from webshop.catalog.models import Product
+from webshop.catalog.models import Product, Cupon
 from webshop.cart.models import CartItem
 
 
 CART_ID_SESSION_KEY = 'cart_id'
+
+# def _cart_id(request):
+#     """
+#       Получение id корзины из cookies для пользователя,
+#       или установка новых cookies если не существуют
+#       _модификатор для видимости в пределах модуля
+#       """
+#     if request.session.get(CART_ID_SESSION_KEY, '') == '':
+#         request.session[CART_ID_SESSION_KEY] = _generate_cart_id()
+#     return request.session[CART_ID_SESSION_KEY]
+#
+# def _generate_cart_id():
+#     """Генерация уникального id корзины который будет хранится в cookies"""
+#     cart_id = ''
+#     characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()'
+#     cart_id_length = 50
+#     for y in range(cart_id_length):
+#         cart_id += characters[random.randint(0, len(characters) - 1)]
+#     return cart_id
+#
+# def get_cart_items(request):
+#     """Получение всех товаров для текущей корзины"""
+#     return CartItem.objects.filter(cart_id=_cart_id(request))
+#
+# def add_to_cart(request):
+#     """Добавление товара в корзину"""
+#     postdata = request.POST.copy()
+#     # Получаем чистое имя товара, возвращает пустую строку если нет
+#     product_slug = postdata.get('product_slug', '')
+#     # Получаем количество добавлеых товаров, возрат 1 если нет
+#     quantity = postdata.get('quantity', 1)
+#     # Получаем товар, или возвращаем ошибку "не найден" если его не существует
+#     p = get_object_or_404(Product, slug=product_slug)
+#     # Получаем товары в корзине
+#     cart_products = get_cart_items(request)
+#     product_in_cart = False
+#     # Проверяем что продукт уже в корзине
+#     for cart_item in cart_products:
+#         if cart_item.product.id == p.id:
+#             # Обновляем количество если найден
+#             cart_item.augment_quantity(quantity)
+#             product_in_cart = True
+#     if not product_in_cart:
+#         # Создаем и сохраняем новую корзину
+#         ci = CartItem()
+#         ci.product = p
+#         ci.quantity = quantity
+#         ci.cart_id = _cart_id(request)
+#         ci.save()
+#
+# def cart_distinct_item_count(request):
+#     """Возвращает общее количество товаров в корзине"""
+#     return get_cart_items(request).count()
+#
+# def get_single_item(request, item_id):
+#     """Получаем конкретный товар в корзине"""
+#     return get_object_or_404(CartItem, id=item_id, cart_id=_cart_id(request))
+#
+# def update_cart(request):
+#     """Обновляет количество отдельного товара"""
+#     postdata = request.POST.copy()
+#     item_id = postdata['item_id']
+#     quantity = postdata['quantity']
+#     cart_item = get_single_item(request, item_id)
+#     if cart_item:
+#         if quantity.isdigit() and int(quantity) > 0:
+#             cart_item.quantity = int(quantity)
+#             cart_item.save()
+#     #else:
+#     #TODO: добавить предупреждение
+#     #    remove_from_cart(request)
+#
+# def remove_from_cart(request):
+#     """Удаляет выбранный товар из корзины"""
+#     postdata = request.POST.copy()
+#     item_id = postdata['item_id']
+#     cart_item = get_single_item(request, item_id)
+#     if cart_item:
+#         cart_item.delete()
+#
+# def cart_subtotal(request):
+#     """Получение суммарной стоимости всех товаров"""
+#     cart_total = decimal.Decimal('0.00')
+#     cart_products = get_cart_items(request)
+#     for cart_item in cart_products:
+#         cart_total += cart_item.product.price * cart_item.quantity
+#     return cart_total
+#
+# def is_empty(request):
+#     """Если корзина пустая возвращаем True"""
+#     return cart_distinct_item_count(request) == 0
+#
+# def empty_cart(request):
+#     """Очищает корзину покупателя"""
+#     user_cart = get_cart_items(request)
+#     user_cart.delete()
+
 
 def _cart_id(request):
     """
@@ -45,6 +142,7 @@ def add_to_cart(request):
     p = get_object_or_404(Product, slug=product_slug)
     # Получаем товары в корзине
     cart_products = get_cart_items(request)
+    cupon = get_cupon(request)
     product_in_cart = False
     # Проверяем что продукт уже в корзине
     for cart_item in cart_products:
@@ -58,6 +156,7 @@ def add_to_cart(request):
         ci.product = p
         ci.quantity = quantity
         ci.cart_id = _cart_id(request)
+        ci.cupon = cupon
         ci.save()
 
 def cart_distinct_item_count(request):
@@ -82,6 +181,26 @@ def update_cart(request):
     #TODO: добавить предупреждение
     #    remove_from_cart(request)
 
+def update_cupon_cart(request):
+    cart_products = get_cart_items(request)
+    postdata = request.POST.copy()
+    cupon_code ='%s' % postdata['cupon']
+    try:
+        cupon_true = Cupon.objects.get(identifier=cupon_code)
+    except Exception:
+        cupon_true = Cupon.objects.get(identifier='zero')
+    for cart_item in cart_products:
+        cart_item.cupon = cupon_true
+        cart_item.save()
+
+def get_cupon(request):
+    cart_items = get_cart_items(request)
+    cupon = Cupon.objects.get(identifier='default')
+    if cart_items:
+        for item in cart_items:
+            cupon = item.cupon
+    return cupon
+
 def remove_from_cart(request):
     """Удаляет выбранный товар из корзины"""
     postdata = request.POST.copy()
@@ -95,7 +214,7 @@ def cart_subtotal(request):
     cart_total = decimal.Decimal('0.00')
     cart_products = get_cart_items(request)
     for cart_item in cart_products:
-        cart_total += cart_item.product.price * cart_item.quantity
+        cart_total += (cart_item.product.price - (cart_item.product.price * int(cart_item.cupon.percent) / 100)) * cart_item.quantity
     return cart_total
 
 def is_empty(request):
--- webshop/catalog/models.py
+++ webshop/catalog/models.py
@@ -205,4 +205,13 @@ class Characteristic(models.Model):
         unique_together = (('product', 'characteristic_type'),)
 
 
+class Cupon(models.Model):
+    name = models.CharField(verbose_name=u'Название купона', max_length=256)
+    identifier = models.CharField(verbose_name=u'Идентификатор', max_length=256)
+    percent = models.CharField(verbose_name=u'Процент скидки', max_length=10)
 
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = u'Система купонов'
