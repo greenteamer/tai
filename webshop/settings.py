a6dd9ece08e8c85d163b2ea95ea281fc471caec2
greenteamer <greenteamer@bk.ru>
2014-12-03 13:55:46 +0600

    settings ckeditor

--- webshop/settings.py
+++ webshop/settings.py
@@ -166,10 +166,6 @@ SOUTH_MIGRATION_MODULES = {
 }
 
 
-CKEDITOR_UPLOAD_PATH = "uploads/"
-CKEDITOR_JQUERY_URL = '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'
-
-
 INSTALLED_APPS = (
     'admin_tools',
     'admin_tools.theming',
